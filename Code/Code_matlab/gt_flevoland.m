d57570e7047bd07b6bdfc59461489c64f1b5c270
anderborba <anderborba@gmail.com>
2020-03-18 22:44:40 -0300

    versao 9 dia 18-03-2020

---
+++ Code/Code_matlab/gt_flevoland.m
@@ -0,0 +1,102 @@
+clear all;
+format long;
+cd ..
+cd ..
+cd Data
+load AirSAR_Flevoland_Enxuto.mat
+[nrows, ncols, nc] = size(S);
+cd ..
+cd Code/Code_matlab
+for i =1: nrows
+	for j = 1: ncols
+     		I11(i, j)   = S(i, j, 1);
+     		I22(i, j)   = S(i, j, 2);
+     		I33(i, j)   = S(i, j, 3);
+     		SS(i, j, 1)  = sqrt(S(i, j, 4)^2 + S(i, j, 7)^2);
+     		SS(i, j, 2)  = sqrt(S(i, j, 5)^2 + S(i, j, 8)^2);
+     		SS(i, j, 3)  = sqrt(S(i, j, 6)^2 + S(i, j, 9)^2);
+	end
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+II = show_Pauli(S, 1, 0);
+%%%%%%%%%%%%%%%%%%%i%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+m = 750;
+n = 1024;
+GT = zeros(m, n);
+%figure(1), imshow(I255);
+%figure(2), imshow(IG);
+%%%%%%%%%%% ROIs %%%%%%%%%%%%%%%%%%
+imshow(II)
+%
+axis on
+hold on;
+impixelinfo;
+
+%dpixel = size(xpixel);
+%for i= 1: dpixel(1)
+%			plot(ypixel(i), xpixel(i),'ro',...
+%    				'LineWidth',1.0,...
+%    				'MarkerSize',3.5,...
+%    				'MarkerEdgeColor',[0.85 0.325 0.089],...
+%    				'MarkerFaceColor', [0.85 0.325 0.089])
+%end	
+%for i= 181:220
+%plot(i, 287,'ro',...
+%    				'LineWidth',1.0,...
+%    				'MarkerSize',3.5,...
+%    				'MarkerEdgeColor',[0.85 0.325 0.089],...
+%    				'MarkerFaceColor', [0.85 0.325 0.089])
+%end	
+%for i= 221:268
+%plot(i, 286,'ro',...
+%    				'LineWidth',1.0,...
+%    				'MarkerSize',3.5,...
+%    				'MarkerEdgeColor',[0.85 0.325 0.089],...
+%    				'MarkerFaceColor', [0.85 0.325 0.089])
+%end	
+for i= 159:310
+GT(287, i) = 1;
+plot(i, 287,'ro',...
+    				'LineWidth',1.0,...
+    				'MarkerSize',3.5,...
+    				'MarkerEdgeColor',[0.85 0.325 0.089],...
+    				'MarkerFaceColor', [0.85 0.325 0.089])
+end	
+for j= 288:330
+GT(j, 159) = 1;	
+plot(159, j,'ro',...
+    				'LineWidth',1.0,...
+    				'MarkerSize',3.5,...
+    				'MarkerEdgeColor',[0.85 0.325 0.089],...
+    				'MarkerFaceColor', [0.85 0.325 0.089])
+end	
+for i= 159:310
+GT(331, i) = 1;
+plot(i, 331,'ro',...
+    				'LineWidth',1.0,...
+    				'MarkerSize',3.5,...
+    				'MarkerEdgeColor',[0.85 0.325 0.089],...
+    				'MarkerFaceColor', [0.85 0.325 0.089])
+end	
+for j= 288:331
+GT(j, 310) = 1;
+plot(310, j,'ro',...
+    				'LineWidth',1.0,...
+    				'MarkerSize',3.5,...
+    				'MarkerEdgeColor',[0.85 0.325 0.089],...
+    				'MarkerFaceColor', [0.85 0.325 0.089])
+end	
+cd ..
+cd ..
+cd Data
+fname = sprintf('gt_flevoland.txt');
+fid = fopen(fname,'w');
+for i = 1: m
+	for j = 1: n
+                fprintf(fid,'%f ', GT(i,j));
+        end
+        fprintf(fid,'\r\n');
+end
+fclose(fid); 
+cd ..
+cd Code/Code_matlab
---
+++ Code/Code_r/metricas_fusão_flev.r
@@ -0,0 +1,56 @@
+# AAB - versao 1.0 17/11/2018
+# O programa le as evidencia de bordas para cada canal no diretorio ~/Data e calcula a probabilidade de detecçao de borda e imprime para arquivo em /Text/Dissertacao/figuras
+# Obs (1) Trocar os nomes das amostras de interesse na leitura de dados,
+#     (2) trocar os nomes do arquivo de impressão, 
+#     (3) amostras definidas em \cite{nhfc} e \cite{gamf},
+#     (4) impressão comentada.
+rm(list = ls())
+require(ggplot2)
+require(latex2exp)
+#
+setwd("../..")
+setwd("Data")
+mat <- scan('metricas_fusao_flevoland.txt')
+setwd("..")
+setwd("Code/Code_r")
+mat <- matrix(mat,  ncol = 10, byrow = TRUE)
+nk <- 10
+x       <- seq(0, nk - 1, 1)
+freq_f1 <- matrix(0, 0, nk)
+freq_f2 <- matrix(0, 0, nk)
+freq_f3 <- matrix(0, 0, nk)
+freq_f4 <- matrix(0, 0, nk)
+freq_f5 <- matrix(0, 0, nk)
+freq_f6 <- matrix(0, 0, nk)
+for(k in 1: nk){
+	freq_f1[k] <- mat[1, k]
+	freq_f2[k] <- mat[2, k]
+	freq_f3[k] <- mat[3, k]
+	freq_f4[k] <- mat[4, k]
+	freq_f5[k] <- mat[5, k]
+	freq_f6[k] <- mat[6, k]
+}
+df <- data.frame(x = x, y1 = freq_f1, y2 = freq_f2, y3 = freq_f3, y4 = freq_f4, y5 = freq_f5, y6 = freq_f6)
+alpha <- c(1,2,3,4,5,6)
+p <- ggplot(df) 
+#  Retirei para reconfigurar para os artigos em ingles
+##pp <- p + geom_line(aes(x = x, y = y1, color = "Ihh"))+  geom_line(aes(x = x, y = y2, color = "Ihv"))+  geom_line(aes(x = x, y = y3, color = "Ivv")) + scale_y_log10(limits = c(0.002, 1))+ ggtitle(TeX('Probabilidade de detecção das evidências de bordas')) + ylab(TeX('Probabilidade de detecção estimada')) + xlab(TeX('Erros de detecção')) 
+pp <- p + geom_line(aes(x = x, y = y1, color = "Aver")) +
+          geom_line(aes(x = x, y = y2, color = "PCA")) +
+	  geom_line(aes(x = x, y = y4, color = "MR-DWT")) +
+          geom_line(aes(x = x, y = y3, color = "MR-SWT")) +
+          geom_line(aes(x = x, y = y5, color = "ROC")) +
+          geom_line(aes(x = x, y = y6, color = "MR-SVD")) +
+	 scale_y_log10(limits = c(0.002, 1)) +
+	 ylab(TeX('Probability')) +
+	 xlab(TeX('Detection error')) 
+print(pp) 
+setwd("../..")
+setwd("Text/Dissertacao/figuras")
+ggsave("metricas_6_fusao_flevoland.pdf")
+setwd("../../..")
+setwd("Code/Code_r")
+
+
+
+
