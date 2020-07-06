27c0f7f734d59a5a3fd5e78eb35b1a5f8d626963
anderborba <anderborba@gmail.com>
2020-07-06 14:31:01 -0300

    revisao artigo GRSL-2020-06-julho-14h30

--- Code/Code_art_grsl_2020_tengarss/Code_r/metricas_fusão_flev.r
+++ Code/Code_art_grsl_2020_tengarss/Code_r/metricas_fusão_flev.r
@@ -4,6 +4,7 @@
 #     (2) trocar os nomes do arquivo de impressão, 
 #     (3) amostras definidas em \cite{nhfc} e \cite{gamf},
 #     (4) impressão comentada.
+#   flevoland r3
 rm(list = ls())
 require(ggplot2)
 require(latex2exp)
@@ -15,7 +16,7 @@ setwd("../../..")
 setwd("Data")
 mat <- scan('metricas_fusao_flevoland_r3.txt')
 setwd("..")
-setwd("Code/Code_art_grsl_2020_tengarss/Code_matlab")
+setwd("Code/Code_art_grsl_2020_tengarss/Code_r")
 mat <- matrix(mat,  ncol = 10, byrow = TRUE)
 nk <- 10
 x       <- seq(0, nk - 1, 1)
@@ -38,17 +39,17 @@ alpha <- c(1,2,3,4,5,6)
 p <- ggplot(df) 
 #  Retirei para reconfigurar para os artigos em ingles
 ##pp <- p + geom_line(aes(x = x, y = y1, color = "Ihh"))+  geom_line(aes(x = x, y = y2, color = "Ihv"))+  geom_line(aes(x = x, y = y3, color = "Ivv")) + scale_y_log10(limits = c(0.002, 1))+ ggtitle(TeX('Probabilidade de detecção das evidências de bordas')) + ylab(TeX('Probabilidade de detecção estimada')) + xlab(TeX('Erros de detecção')) 
-pp <- p + geom_line(aes(x = x, y = y1, color = "Aver")  , size=2, alpha=.7) +
-          geom_line(aes(x = x, y = y2, color = "PCA")   , size=2, alpha=.7, linetype = 3) +
-	        geom_line(aes(x = x, y = y4, color = "MR-DWT"), size=2, alpha=.7) +
-          geom_line(aes(x = x, y = y3, color = "MR-SWT"), size=2, alpha=.7) +
-          geom_line(aes(x = x, y = y5, color = "ROC")   , size=2, alpha=.7) +
-          geom_line(aes(x = x, y = y6, color = "MR-SVD"), size=2, alpha=.7) +
+pp <- p + geom_line(aes(x = x, y = y1, color = "Aver")  , size=4, alpha=.7) +
+          geom_line(aes(x = x, y = y2, color = "PCA")   , size=4, alpha=.7, linetype = 3) +
+	        geom_line(aes(x = x, y = y4, color = "MR-DWT"), size=4, alpha=.7) +
+          geom_line(aes(x = x, y = y3, color = "MR-SWT"), size=4, alpha=.7) +
+          geom_line(aes(x = x, y = y5, color = "ROC")   , size=4, alpha=.7) +
+          geom_line(aes(x = x, y = y6, color = "MR-SVD"), size=4, alpha=.7) +
   ylim(.01,1) +
 	 ylab(TeX('Probability')) +
 	 xlab(TeX('Detection error')) +
   theme_ipsum(base_family = "Times New Roman", 
-              base_size = 10, axis_title_size = 10) +
+              base_size = 20, axis_title_size = 20) +
   scale_fill_ipsum() +
   theme(legend.title = element_blank()) +
   theme(plot.margin=grid::unit(c(0,0,0,0), "mm"),
