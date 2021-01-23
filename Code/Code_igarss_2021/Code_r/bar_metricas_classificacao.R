720e5d0be7902f46a20a0c13496b15d0f7b3f368
anderborba <anderborba@gmail.com>
2021-01-23 15:05:56 -0300

    versão 2 - Art igarss 2021-23-01-2021:15h05min

--- Code/Code_igarss_2021/Code_r/bar_metricas_classificacao.R
+++ Code/Code_igarss_2021/Code_r/bar_metricas_classificacao.R
@@ -25,37 +25,46 @@ for(k in 1: nk){
   v3[k] <- mat[3, k]
 }
 df <- data.frame(x = eixo, y1 =v1, y2 = v2, y3 = v3)
-
-ggplot(data=df, aes(x=eixo, y=y3)) +
-  ylim(0, .65) +
-  geom_bar(stat="identity",  fill="steelblue")+
-  xlab("Métodos de detecção de bordas")+
-  ylab("Valor")+
-  ggtitle(TeX("Métrica Mcc para os métodos de detecção de bordas")) +
-  geom_text(aes(label=y3), vjust=-0.3, color="blue", size=3.5) +
-  theme_clean() +
-  theme(axis.text.x=element_text(angle = 90, vjust = 0.5, hjust=1))
+#ggplot(data=df, aes(x=eixo, y=y3)) +
+#  ylim(0, .65) +
+#  geom_bar(stat="identity",  fill="steelblue")+
+#  xlab("Métodos de detecção de bordas")+
+#  ylab("Valor")+
+#  ggtitle(TeX("Métrica Mcc para os métodos de detecção de bordas")) +
+#  geom_text(aes(label=y3), vjust=-0.3, color="blue", size=3.5) +
+#  theme_clean() +
+#  theme(axis.text.x=element_text(angle = 90, vjust = 0.5, hjust=1))
 
 # Gráfico de métricas relativas
-
 df$GanhoRelativo <- (df$y3-min(df$y3))/(1-min(df$y3))
-
-p <- ggplot(data=df, 
-       aes(x=reorder(eixo, GanhoRelativo), 
-           y=GanhoRelativo
+df$GanhoRelativoMac <- (df$y1-min(df$y1))/(1-min(df$y1))
+df$GanhoRelativoMfe <- (df$y2-min(df$y2))/(1-min(df$y2))
+df$GanhoRelativoMcc <- (df$y3-min(df$y3))/(1-min(df$y3))
+#
+ggplot(data=df, 
+       aes(x=reorder(eixo, GanhoRelativoMcc), 
+           y=GanhoRelativoMcc
            )
        ) + 
   geom_bar(stat="identity",  fill="steelblue")+
-  labs(title="Métrica nMcc para os métodos de detecção de bordas",
-       subtitle="Melhoria relativa à pior métrica: F-ROC 51.19%",
-       x="Métodos de detecção de bordas",
-       y="Ganho relativo") +
+  #labs(title="Métrica nMcc para os métodos de detecção de bordas",
+  #     subtitle="Melhoria relativa à pior métrica: F-ROC 51.19%",
+  #     x="Métodos de detecção de bordas",
+  #     y="Ganho relativo") +
+  labs(title="nMcc metrics for edge detection methods",
+       subtitle="Improvement relative to the worst metric: F-MR-DWT 51.19%",
+       x="Edge Detection Methods",
+       y="Relative Gain") +
   geom_text(aes(
-            label=paste(100*round(GanhoRelativo, 4), "%"),
+            label=paste(100*round(GanhoRelativoMcc, 4), "%"),
             ), 
             hjust=0, vjust=0, color="blue", size=3.5) +
-#  xlim(0, .25) +
-  coord_flip() +
+  coord_flip(ylim = c(0, 0.25)) +
   theme_clean() +
   theme(axis.text.x=element_text(angle = 90, vjust = 0.5, hjust=1))
-print(p)
+#
+setwd("../../..")
+setwd("Text/Dissertacao/figuras")
+ggsave("metrica_igarss_2021_mcc.pdf")
+setwd("../../..")
+setwd("Code/Code_igarss_2021/Code_r")
--- Code/Code_igarss_2021/Code_r/bar_metricas_classificacao_ingles.R
+++ Code/Code_igarss_2021/Code_r/bar_metricas_classificacao_ingles.R
@@ -26,27 +26,56 @@ for(k in 1: nk){
 }
 df <- data.frame(x = eixo, y1 =v1, y2 = v2, y3 = v3)
 # Gráfico de métricas relativas
-#df$GanhoRelativo <- (df$y3-min(df$y3))/(1-min(df$y3))
+df$GanhoRelativo <- (df$y3-min(df$y3))/(1-min(df$y3))
 df$GanhoRelativoMac <- (df$y1-min(df$y1))/(1-min(df$y1))
 df$GanhoRelativoMfe <- (df$y2-min(df$y2))/(1-min(df$y2))
 df$GanhoRelativoMcc <- (df$y3-min(df$y3))/(1-min(df$y3))
-
-p <- ggplot(data=df, 
-       aes(x=reorder(eixo, GanhoRelativoMcc), 
-           y=GanhoRelativoMcc
-           )
-       ) + 
-  geom_bar(stat="identity",  fill="lightsteelblue2")+
-  labs(title="nMcc metric for edge detection methods",
+#
+ggplot(data=df, 
+       aes(x=reorder(eixo, GanhoRelativo), 
+           y=GanhoRelativo
+       )
+) + 
+  geom_bar(stat="identity",  fill="steelblue")
+  labs(title="nMcc metrics for edge detection methods",
        subtitle="Improvement relative to the worst metric: F-ROC 51.19%",
        x="Edge Detection Methods",
        y="Relative Gain") +
   geom_text(aes(
-            label=paste(100*round(GanhoRelativoMcc, 4), "%"),
-            ), 
-            hjust=1, vjust=0, color="blue4", size=3.5) +
-  #xlim(0, 1.25) +
-  coord_flip() +
+    label=paste(100*round(GanhoRelativo, 4), "%"),
+  ), 
+  hjust=0, vjust=0, color="blue", size=3.5) +
+  coord_flip(ylim = c(0, 0.25)) +
   theme_clean() +
-  theme(axis.text.x=element_text(angle = 90, vjust = 0.5, hjust=1.0))
-print(p)
+  theme(axis.text.x=element_text(angle = 90, vjust = 0.5, hjust=1))
+#
+#setwd("../../..")
+#getwd()
+#setwd("Text/Dissertacao/figuras")
+#getwd()
+#ggsave("metrica_igarss_2021_mcc.pdf")
+ggsave("teste.pdf")
+#setwd("../../..")
+#getwd()
+#setwd("Code/Code_igarss_2021/Code_r")
+#getwd()
+
+#p <- ggplot(data=df, 
+#       aes(x=reorder(eixo, GanhoRelativoMcc), 
+#           y=GanhoRelativoMcc
+#           )
+#       ) + 
+#  geom_bar(stat="identity",  fill="lightsteelblue2")+
+#  labs(title="nMcc metric for edge detection methods",
+#       subtitle="Improvement relative to the worst metric: F-ROC 51.19%",
+#       x="Edge Detection Methods",
+#       y="Relative Gain") +
+#  geom_text(aes(
+#            label=paste(100*round(GanhoRelativoMcc, 4), "%"),
+#            ), 
+#            hjust=1, vjust=0, color="blue4", size=3.5) +
+#  #xlim(0, 1.25) +
+#  coord_flip() +
+#  theme_clean() +
+#  theme(axis.text.x=element_text(angle = 90, vjust = 0.5, hjust=1.0))
+#print(p)
