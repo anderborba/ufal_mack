a9aee623e40f309afd946e3bd3960b25654377ac
Alejandro C. Frery <acfrery@gmail.com>
2023-08-24 06:55:20 +1200

    Injecting speckle

--- Code/Road Detection/AddSpeckle.R
+++ Code/Road Detection/AddSpeckle.R
@@ -1,13 +1,22 @@
 # Adding speckle to the three channels of a TIFF image
 
-library(rstudioapi)
-library(tiff)
 library(GGally)
+library(tiff)
+source("imagematrix.R")
+set.seed(1234567890, kind="Mersenne-Twister")
 
-setwd(dirname(getActiveDocumentContext()$path)) 
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) 
 Clean <- readTIFF(source="../../Images/TIFF/10078675_15.tiff")
 Clean.df <- data.frame(r=as.vector(Clean[,,1]),
                        g=as.vector(Clean[,,2]),
                        b=as.vector(Clean[,,3]))
+
 summary(Clean.df)
 ggpairs(Clean.df)
+
+# The blue channel usually has the least amount of information
+plot(imagematrix(Clean[,,3]))
+
+# Inject single-look speckle
+SpeckledBlue <- Clean[,,3]*rexp(n=prod(dim(Clean[,,3])), rate=1)
+plot(imagematrix(clipping(SpeckledBlue)))
